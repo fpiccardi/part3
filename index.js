fd40c9c000569e1049eb1662521065bf8f6f45aa
Fil <filippo.piccardi@outlook.com>
2024-11-29 16:22:42 +0100

    3.3

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${
       persons.length
-    } people</p></br><p>${Date()}</p>`
+    } people</p><br/><p>${Date()}</p>`
   );
 });
 
